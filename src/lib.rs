d87ae8241b79caa0656c4a47691fd7a982febb54
Zhen Kyle <zhenkyle@gmail.com>
2020-01-22 11:36:51 +0800

    Add Draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug)]
 pub struct Post {
     state: Box<dyn State>,
@@ -7,15 +9,21 @@ pub struct Post {
 impl Post {
     pub fn new() -> Post {
         Post {
-            state: ???,
+            state: Box::new(Draft{}),
             content: String::from("abc"),
         }
     }
 }
 
 trait State {}
+impl fmt::Debug for dyn State {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "State")
+    }
+}
 
+#[derive(Debug)]
 struct Draft {}
 
-impl trait State for Draft {
+impl State for Draft {
 }
