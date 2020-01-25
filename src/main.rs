771be7dca18e0c43eb69dea279b719d3e35d5d84
Zhen Kyle <zhenkyle@gmail.com>
2020-01-25 15:37:02 +0800

    Rsponed with HTTP 200 or HTTP 404 page

--- src/main.rs
+++ src/main.rs
@@ -21,5 +21,9 @@ fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
     println!("{}",String::from_utf8_lossy(&buffer[..]));
-    stream.write(b"ABC").unwrap();
+    if String::from_utf8_lossy(&buffer[..]).starts_with("GET / HTTP/1.1") {
+        stream.write(b"HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\n\n<html><body><h1>Hello world!</h1></body></html>\n").unwrap();
+    } else {
+        stream.write(b"HTTP/1.1 404 Not Found\nContent-Type: text/html; charset=UTF-8\n\n<html><body><h1>Not Found!</h1></body></html>\n").unwrap();
+    }
 }
