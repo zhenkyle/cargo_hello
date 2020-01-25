58a255e823f78a7eb4770231e1c5a600dea49120
Zhen Kyle <zhenkyle@gmail.com>
2020-01-25 21:16:47 +0800

    Refactoring: learned from THE BOOK

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::io::prelude::*; // for std:io:Read etc.
-use std::fs::File;
+use std::fs;
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
 
     for stream in listener.incoming() {
-        handle_client(stream.unwrap());
+        let stream = stream.unwrap();
+        handle_client(stream);
     }
 }
 // My version also works
@@ -19,21 +20,20 @@ fn handle_client(mut stream: TcpStream) {
  */
 
 fn handle_client(mut stream: TcpStream) {
-    let mut file = File::open("200.html").unwrap();
-    let mut text_200 = String::new();
-    file.read_to_string(&mut text_200).unwrap();
-    file = File::open("404.html").unwrap();
-    let mut text_404 = String::new();
-    file.read_to_string(&mut text_404).unwrap();
-
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!("{}",String::from_utf8_lossy(&buffer[..]));
-    if String::from_utf8_lossy(&buffer[..]).starts_with("GET / HTTP/1.1") {
-        stream.write(b"HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\n\n").unwrap();
-        stream.write(text_200.as_bytes()).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
-        stream.write(b"HTTP/1.1 404 Not Found\nContent-Type: text/html; charset=UTF-8\n\n").unwrap();
-        stream.write(text_404.as_bytes()).unwrap();
-    }
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response=format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
