2961f8c12aeae28eb2c8c4e016d5e4ae7fa6768f
Zhen Kyle <zhenkyle@gmail.com>
2020-01-25 15:56:13 +0800

    Read html contents from files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::prelude::*; // for std:io:Read etc.
+use std::fs::File;
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
@@ -18,12 +19,21 @@ fn handle_client(mut stream: TcpStream) {
  */
 
 fn handle_client(mut stream: TcpStream) {
+    let mut file = File::open("200.html").unwrap();
+    let mut text_200 = String::new();
+    file.read_to_string(&mut text_200).unwrap();
+    file = File::open("404.html").unwrap();
+    let mut text_404 = String::new();
+    file.read_to_string(&mut text_404).unwrap();
+
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
     println!("{}",String::from_utf8_lossy(&buffer[..]));
     if String::from_utf8_lossy(&buffer[..]).starts_with("GET / HTTP/1.1") {
-        stream.write(b"HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\n\n<html><body><h1>Hello world!</h1></body></html>\n").unwrap();
+        stream.write(b"HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\n\n").unwrap();
+        stream.write(text_200.as_bytes()).unwrap();
     } else {
-        stream.write(b"HTTP/1.1 404 Not Found\nContent-Type: text/html; charset=UTF-8\n\n<html><body><h1>Not Found!</h1></body></html>\n").unwrap();
+        stream.write(b"HTTP/1.1 404 Not Found\nContent-Type: text/html; charset=UTF-8\n\n").unwrap();
+        stream.write(text_404.as_bytes()).unwrap();
     }
 }
