616846178e357830b6a76ae0a575cdc1d3404cb2
zhenkyle <zhenkyle@gmail.com>
2020-01-26 10:53:08 +0800

    Figuare out how to add a JoinHandle<()> as a ThreadPool field

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
+use std::thread::JoinHandle;
+
 pub struct ThreadPool {
+    thread: JoinHandle<()>,
 }
 
 impl ThreadPool {
     pub fn new() -> ThreadPool {
-        ThreadPool {}
+        let thread = std::thread::spawn(|| {});
+        ThreadPool {thread: thread}
     }
 
     pub fn execute<F>(&self, f: F)
