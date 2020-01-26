be45e148f8dff9a3941aa6498686b240aecc33fb
zhenkyle <zhenkyle@gmail.com>
2020-01-26 13:32:54 +0800

    Add channel in ThreadPool to be used between parent thread and children threads, parent as sender, children as receivers

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,25 @@
 use std::thread::JoinHandle;
+use std::sync::mpsc::channel;
 
 pub struct ThreadPool {
     threads: Vec<JoinHandle<()>>,
+    sender: std::sync::mpsc::Sender<()>,
 }
 
 impl ThreadPool {
     pub fn new() -> ThreadPool {
+        let (sender, receiver) = channel();
         let mut threads = Vec::with_capacity(4);
         for i in 0..4 {
-            let thread = std::thread::spawn(|| {});
+            let thread = std::thread::spawn(|| {
+            });
             threads.push(thread);
         }
-        ThreadPool {threads}
+
+        ThreadPool {
+            threads,
+            sender
+        }
     }
 
     pub fn execute<F>(&self, f: F)
