8bc3e4c65d379ca2398c929bc0691e4d6803e4c4
zhenkyle <zhenkyle@gmail.com>
2020-01-26 13:49:44 +0800

    Found the way for sharing the single receiver in spawned threads

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,25 @@
 use std::thread::JoinHandle;
 use std::sync::mpsc::channel;
+use std::sync::{Mutex, Arc}; 
 
 pub struct ThreadPool {
     threads: Vec<JoinHandle<()>>,
-    sender: std::sync::mpsc::Sender<()>,
+    sender: std::sync::mpsc::Sender<u32>,
 }
 
 impl ThreadPool {
     pub fn new() -> ThreadPool {
         let (sender, receiver) = channel();
+        let receiver = Mutex::new(receiver);
+        let receiver = Arc::new(receiver);
         let mut threads = Vec::with_capacity(4);
         for i in 0..4 {
-            let thread = std::thread::spawn(|| {
+            let receiver = Arc::clone(&receiver);
+            let thread = std::thread::spawn(move || {
+                loop {
+                    let num = receiver.lock().unwrap().recv().unwrap();
+                    println!("Got {}!", num);
+                }
             });
             threads.push(thread);
         }
@@ -27,5 +35,6 @@ impl ThreadPool {
         F: FnOnce(),
         F: Send + 'static,
     {
+        self.sender.send(5).unwrap();
     }
 }
